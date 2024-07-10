232d8a703bef1bf11e7372b060a12a8ac368c118
Alfredo <aoshiken@users.noreply.github.com>
2024-07-10 11:27:57 +0200

    Fix compatibility with Python >= 3.11

--- rekall_yara/yara-python/yara-python.c
+++ rekall_yara/yara-python/yara-python.c
@@ -14,6 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 /* headers */
+#define PY_SSIZE_T_CLEAN
 
 #include <Python.h>
 #include "structmember.h"
